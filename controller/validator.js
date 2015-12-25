70f1b9914a07267e5f2ab64729ab6a03d840b9be
Arun Joseph <arun@Aruns-MacBook-Air.local>
2015-12-25 11:35:42 -0800

    removed unused commented out code and updated html of lending page

--- controller/validator.js
+++ controller/validator.js
@@ -10,25 +10,6 @@ var Item = require('../models/item');
 
 module.exports = {
 
-    // // Processes the entire form as a lumpsum
-    // processAllFieldsOfTheForm: function(req, res) {
-    //     var form = new formidable.IncomingForm();
-    //     form.parse(req, function (err, fields, files) {
-    //         //Store the data from the fields in your data store.
-    //         //The data store could be a file or database or any other store based
-    //         //on your application.
-    //         res.writeHead(200, {
-    //             'content-type': 'text/plain'
-    //         });
-    //         res.write('received the data:\n\n');
-    //         res.end(util.inspect({
-    //             fields: fields,
-    //             files: files
-    //         }));
-    //         console.log(res);
-    //     });
-    // },
-
     // Processes all the fields of the form, can modify to include Errors, Files, and other parameters
     processFormFieldsIndividual: function(req, res) {
         //Store the data from the fields in your data store.
